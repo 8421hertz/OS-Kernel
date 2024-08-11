58d6defc00df035d5816a7d1da72754fa96d6588
chengMiss <2894014802@qq.com>
2024-08-11 15:08:03 +0800

    实现整数打印

--- OS/kernel/main.c
+++ OS/kernel/main.c
@@ -2,9 +2,18 @@
 // int _start(void)
 int main(void)
 {
-    put_str("I am playing basketball!\n");
-
+    put_str("I am kernel\n");
+    put_int(0);
+    put_char('\n');
+    put_int(9);
+    put_char('\n');
+    put_int(0x00021a3f);
+    put_char('\n');
+    put_int(0x12345678);
+    put_char('\n');
+    put_int(0x00000000);
     while (1)
         ;
+
     return 0;
 }
