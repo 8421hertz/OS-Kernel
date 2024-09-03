bebac45be3e41bedbd6f56a1f7dffe744b7094ce
8421hertz <2894014802@qq.com>
2024-09-03 21:06:37 +0800

    init.c是用来做所有的初始化相关的工作，而中断初始化只是其中之一；main.c中的sti开启了开中断，并且此时仅有IR0对应的始终中断被开启

--- OS/kernel/main.c
+++ OS/kernel/main.c
@@ -1,17 +1,13 @@
 #include "print.h"
+#include "init.h"
 // int _start(void)
 int main(void)
 {
+
     put_str("I am kernel\n");
-    put_int(0);
-    put_char('\n');
-    put_int(9);
-    put_char('\n');
-    put_int(0x00021a3f);
-    put_char('\n');
-    put_int(0x12345678);
-    put_char('\n');
-    put_int(0x00000000);
+    init_all();
+    asm volatile("sti");        // 为演示中断处理，在此临时开中断 (sti 会将标志寄存器 eflags 中的 IF 位置 1 ，这样来自中断代理 8259A 的中断信号便被处理器受理了。)
+
     while (1)
         ;
 
