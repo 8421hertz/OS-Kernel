e93c46b95c316b2d8a042bc48906ff2d79b5be8c
8421hertz <2894014802@qq.com>
2024-09-03 16:28:21 +0800

    设置8259A：①初始化主片和从片 ②设置屏蔽某个外部设备的中断信号（设置 IMR 寄存器只放行时钟中断）

--- OS/kernel/interrupt.c
+++ OS/kernel/interrupt.c
@@ -1,8 +1,14 @@
 #include "interrupt.h"
 #include "stdint.h"
 #include "global.h"
+#include "io.h"
 
 #define IDT_DESC_CNT 0x21 // 目前总共支持的中断数 ( 33 个中断处理程序 )
+
+#define PIC_M_CTRL 0x20 // 主片的控制端口是 0x20
+#define PIC_M_DATA 0x21 // 主片的数据端口是 0x21
+#define PIC_S_CTRL 0xa0 // 从片的控制端口是 0xa0
+#define PIC_S_DATA 0xa1 // 从片的数据端口是 0xa1
 
 /* 中断门描述符结构体 */
 struct gate_desc
@@ -22,6 +28,41 @@ static struct gate_desc idt[IDT_DESC_CNT]; // idt 中断描述符表，本质上
 extern intr_handler intr_entry_table[IDT_DESC_CNT]; // 声明引用定义在 kernel.S 中的中断处理函数入口地址数组
                                                     // 中断描述符地址数组 ( 仅仅表明地址，用于修饰 intr_entry_table，定义在 interrupt.h 中 )
                                                     // 中断向量号 * 8 + IDTR = 中断门描述符地址
+
+/**
+ * @brief 初始化可编程中断控制器 8259A。
+ *
+ * 该函数通过发送指令字 (ICW) 来初始化主片和从片的 8259A 中断控制器。主片和从片负责管理中断向量，并通过主片的 IR2 引脚进行级联。
+ *
+ * 初始化步骤包括：
+ * 1. 启动主片和从片的边沿触发模式。
+ * 2. 设置中断向量号范围。
+ * 3. 设定主片和从片之间的级联关系。
+ * 4. 打开 IRQ0 ，只接受时钟中断。
+ *
+ * @param void 该函数不接受任何参数。
+ * @return void 该函数无返回值。
+ */
+static void pic_init(void)
+{
+    /* 初始化主片 */
+    outb(PIC_M_CTRL, 0x11); // ICW1 ：主片边沿出发；级联从片；需要 ICW4
+    outb(PIC_M_DATA, 0x20); // ICW2 ：设置主片的起始中断向量号为 0x20 ，代表 IR[0~7] 为 0x20~0x27
+    outb(PIC_M_DATA, 0x04); // ICW3 ：使用主片中的 IR2 引脚级联从片
+    outb(PIC_M_DATA, 0x01); // ICW4 ：开启 x86 模式；开启 EOI
+
+    /* 初始化从片 */
+    outb(PIC_S_CTRL, 0x11);
+    outb(PIC_S_DATA, 0x28); // ICW2 ：设置从片的起始中断向量号为 0x28 ，代表 IR[8~15] 为 0x28~0x2F
+    outb(PIC_S_DATA, 0x02);
+    outb(PIC_S_DATA, 0x01);
+
+    /* 利用 OCW1 设置 IMR 寄存器只放行时钟中断 ( 也就是目前只接收时钟产生的中断 ) */
+    outb(PIC_M_DATA, 0xFE);
+    outb(PIC_S_DATA, 0xFF);
+
+    put_str("   pic_init done\n");
+}
 
 /**
  * @brief 创建中断门描述符（在中断描述符表 IDT 中填充中断门描述符）
