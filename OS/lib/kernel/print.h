3d051c1980a1ecaea5ec51a44143d3208c0f9831
8421hertz <2894014802@qq.com>
2024-10-20 22:03:41 +0800

    声明 set_cursor() 设置光标函数供 general_intr_handler 通用中断处理函数调用

--- OS/lib/kernel/print.h
+++ OS/lib/kernel/print.h
@@ -3,5 +3,6 @@
 #include "stdint.h"
 void put_char(uint8_t char_ascii);
 void put_str(char* message);
-void put_int(uint32_t num);         // 以十六进制打印
+void put_int(uint32_t num); // 以十六进制打印
+void set_cursor(uint16_t num);  // 设置光标
 #endif
